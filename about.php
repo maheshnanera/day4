45022af7028a43f9385782c9699c6d6d44a9ae19
Mahesh Nanera <84780820+maheshnanera@users.noreply.github.com>
2021-05-31 16:01:26 +0530

    day4
    day4 task uploded

---
+++ about.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+    <meta name="keywords" content="" />
+    <meta name="description" content="" />
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+    <title>Stampalike by FCT</title>
+    <link href="http://fonts.googleapis.com/css?family=Arvo" rel="stylesheet" type="text/css" />
+    <link href="http://fonts.googleapis.com/css?family=Coda:400,800" rel="stylesheet" type="text/css" />
+    <link href="style.css" rel="stylesheet" type="text/css" media="screen" />
+</head>
+
+<body>
+<div id="menu-wrapper">
+        <div id="menu">
+            <ul>
+                <li class="current_page_item"><a href="index.html">Homepage</a></li>
+
+                <li><a href="photos.php">Photos</a></li>
+                <li><a href="about.php">About</a></li>
+
+                <li><a href="contact.php">Contact</a></li>
+            </ul>
+        </div>
+        <!-- end #menu -->
+    </div>
+    <div id="wrapper">
+        <!-- end #header -->
+        <div id="page-bgtop"></div>
+        <div id="page">
+            <div><img src="123.jpeg" width="620" height="400" alt="" /></div>
+
+            <div id="content">
+                <div class="post">
+                    <h2 class="title"><a href="#">nmr ltd </a></h2>
+                    <p class="meta"><span class="date">June 04, 2020 My name is <strong>MAHESH NANERA</strong> from <strong> ltd</</span><span class="posted">Posted by <a href="#">Someone</a></span></p>
+                    <div style="clear: both;">&nbsp;</div>
+
+                </div>
+
+                <div style="clear: both;">&nbsp;</div>
+            </div>
+            
+            <div id="sidebar">
+
+<marquee>HELLO WELCOME TO NMR LTD</marquee>
+
+
+
+</div>
+</body>
+
+</html>
---
+++ contact.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+    <meta name="keywords" content="" />
+    <meta name="description" content="" />
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+    <title>Stampalike by FCT</title>
+    <link href="http://fonts.googleapis.com/css?family=Arvo" rel="stylesheet" type="text/css" />
+    <link href="http://fonts.googleapis.com/css?family=Coda:400,800" rel="stylesheet" type="text/css" />
+    <link href="style.css" rel="stylesheet" type="text/css" media="screen" />
+</head>
+
+<body>
+    
+<div id="menu-wrapper">
+        <div id="menu">
+            <ul>
+                <li class="current_page_item"><a href="index.html">Homepage</a></li>
+
+                <li><a href="photos.php">Photos</a></li>
+                <li><a href="about.php">About</a></li>
+
+                <li><a href="contact.php">Contact</a></li>
+            </ul>
+        </div>
+        <!-- end #menu -->
+    </div>
+    <div id="wrapper">
+        <!-- end #header -->
+        <div id="page-bgtop"></div>
+        <div id="page">
+            <div><img src="123.jpeg" width="620" height="400" alt="" /></div>
+
+            <div id="content">
+                <div class="post">
+                    <h2 class="title"><a href="#">nmr ltd </a></h2>
+                    <p class="meta"><span class="date">June 04, 2020</span><span class="posted"> <a href="#"></a></span></p>
+                    <div style="clear: both;">&nbsp;</div>
+<p>MAHESH NANERA Contact NO, : 9574731624</p>
+                </div>
+
+                <div style="clear: both;">&nbsp;</div>
+            </div>
+            <!-- end #content -->
+            <div id="sidebar">
+                <ul>
+                    <li>
+                        <h2>SearCh Here:</h2>
+                        <div id="search">
+                            <form method="get" action="#">
+                                <div>
+                                    <input type="text" name="s" id="search-text" value="" />
+                                    <input type="submit" id="search-submit" value="" />
+                                </div>
+                            </form>
+                        </div>
+                        <div style="clear: both;">&nbsp;</div>
+                    </li>
+                    <li>
+                        <h2>Aliquam tempus</h2>
+                        <p>Mauris vitae nisl nec metus placerat perdiet est. Phasellus dapibus semper consectetuer hendrerit.</p>
+                    </li>
+                    <div id="sidebar">
+
+                <marquee>HELLO WELCOME TO NMR LTD</marquee>
+
+               
+               
+            </div>
+                    
+</body>
+
+</html>
