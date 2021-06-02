990ba8d74b7ea39747deff8b6e5b9c75fdf8411e
Mahesh Nanera <84780820+maheshnanera@users.noreply.github.com>
2021-06-02 09:34:48 +0530

    d4
    Theme Integration

---
+++ theme/about.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+    <title>Pastelco by FCT</title>
+    <meta name="keywords" content="" />
+    <meta name="description" content="" />
+    <link href="default.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body>
+    <div id="header">
+        <div id="logo">
+            <h1><a href="#">nmr ltd</a></h1>
+            <h2><span>Managed by NMR LTD.</span></h2>
+        </div>
+        <div id="menu">
+            <ul>
+                <li class="active"><a href="index.html" title="">Homepage</a></li>
+                <li><a href="#" title="">About Us</a></li>
+               
+                <li><a href="contact.php" title="">contact detail</a></li>
+                <li><a href="pass.php" title="">Register</a></li>
+            </ul>
+        </div>
+    </div>
+    <div id="content">
+        <div id="sidebar">
+            <div id="support">
+                <h2>Custemer care NO.</h2>
+                <h3>9574731624</h3>
+               
+            </div>
+            
+            <marquee>WELCOME TO NMR LTD.</marquee>
+        </div>
+        <div id="main">
+            <div id="welcome" class="post">
+                <p><img src="111.jpg" alt="" width="472" height="232" class="portrait" /></p>
+                <h2 class="title">Welcome to NMR LTD.</h2>
+                <h3 class="date"><span class="month">Mar.</span> <span class="day">8</span><span class="year">, 2020</span></h3>
+                <div class="meta">
+                    <p>Posted by Mahesh Nanera <a href="#">categorized</a>, <a href="#">Daily Journal</a><br />
+                        <a href="#">Trackback URI</a> | <a href="#">No Comments</a> | <a href="#">Permalink</a></p>
+                </div>
+                <div class="story">
+                    <p><strong>NMR LTD.</strong> is a free template from <a href="http://www.freecsstemplates.org">FCT</a> released under a <a href="http://creativecommons.org/licenses/by/2.5/">Creative Commons Attribution 2.5 License</a>. The photo
+                        is from <a href="http://www.pdphoto.org/">PDPhoto.org</a>. You're free to use it for both commercial or personal use. I only ask that you link back to my site in some way. <em>Enjoy :)</em></p>
+                </div>
+            </div>
+            
+        </div>
+    </div>
+    <div id="footer">
+        <p id="legal">Copyright &copy; 2020 NMR LTD All Rights Reserved. Managed by <a href="http://www.freecsstemplates.org">FCT</a>.</p>
+        <p id="links"><a href="#">Privacy Policy</a> | <a href="#">Terms of Use</a></p>
+    </div>
+</body>
+
+</html>
